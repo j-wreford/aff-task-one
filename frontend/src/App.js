dcba5a32043c69d08ad713c50765989be312c0fd
Joe <joe.wreford@outlook.com>
2020-02-06 18:49:29 +0000

    Update App add routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
-
 import Login from './component/Login'
-import './resource/App.css';
+import Layout from './component/Layout'
+import './resource/App.css'
 
 function App() {
   return (
-    <Grid
+      <Grid
       container
       alignItems="center"
       justify="center"
       direction="column"
       style={{height: "100vh"}}
     >
-      <Grid item sm={12} md={8} lg={3}>
-        <Login></Login>
-      </Grid>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Layout} />
+          <Route exact path="/login" component={Login} />
+        </Switch> 
+      </Router>
     </Grid>
-  );
+  )
 }
 
-export default App;
+export default App
