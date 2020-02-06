d597f5d52a584c7c41751f2550691aed9760cdc9
Joe <joe.wreford@outlook.com>
2020-02-06 10:31:12 +0000

    Initial commit.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import logo from './resource/logo.svg';
+import './resource/App.css';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">Hello, world!</Button>
+  );
+}
+
+export default App;
