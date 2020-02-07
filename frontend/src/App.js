6c8175d57f34b6383c346dff3cce53c81dc62bc0
Joe <joe.wreford@outlook.com>
2020-02-07 19:04:54 +0000

    Update App. Add theme.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,40 @@
-import React from 'react'
+// react
+import React, {useState} from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+// material ui
+import { ThemeProvider } from '@material-ui/styles'
 import {Grid, CssBaseline} from '@material-ui/core'
+
+// application
 import Login from './component/Login'
 import Layout from './component/Layout'
 import Styles from './resource/App.css'
+import theme from './resource/theme'
 
-function App() {
 
-  return (
-    <Grid
-        container
-        alignItems="center"
-        justify="center"
-        direction="column"
-        style={{height: "100vh"}}
-    >
-        <CssBaseline />
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Layout} />
-                <Route exact path="/login" component={Login} />
-            </Switch> 
-        </Router>
-    </Grid>
-  )
+/**
+ * Root component for TMS Media Manager
+ */
+export default function App() {
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Grid
+                container
+                alignItems="center"
+                justify="center"
+                direction="column"
+                style={{height: "100vh"}}
+            >
+                <CssBaseline />
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Layout} />
+                        <Route exact path="/login" component={Login} />
+                    </Switch> 
+                </Router>
+            </Grid>
+        </ThemeProvider>
+    )
 }
-
-export default App
