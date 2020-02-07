15a8f6cd7454c1e4c3492fe579eb2c5358779189
Joe <joe.wreford@outlook.com>
2020-02-07 20:37:50 +0000

    Update Login.

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
-import {
-    Grid, Paper, Box, FilledInput, OutlinedInput, InputLabel, InputAdornment,
-    FormControl, Button, IconButton, Typography
-} from '@material-ui/core'
-import {Visibility, VisibilityOff} from '@material-ui/icons'
+"use strict"
 
-export default class Login extends Component {
+// react
+import React from 'react'
+
+// material ui
+import { Paper, Box, OutlinedInput, InputLabel, InputAdornment, FormControl, Button, IconButton, Typography } from '@material-ui/core'
+import { Visibility, VisibilityOff } from '@material-ui/icons'
+
+/**
+ * Presents an interface for users to authenticate themselves and log
+ * in to the application
+ */
+export default function Login(props) {
 
     /**
-     * Constructor
+     * The value of the username text input
      */
-    constructor() {
-        super()
+    const [username, setUsername] = React.useState("");
 
-        this.state = {
-            username: "",
-            password: "",
-            showPassword: false
-        }
+    /**
+     * The value of the password text input 
+     */
+    const [password, setPassword] = React.useState("");
 
-        this.handleOnChangeUsername = this.handleOnChangeUsername.bind(this);
-        this.handleOnChangePassword = this.handleOnChangePassword.bind(this);
-        this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
-        this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
-        this.handleOnFormSubmit = this.handleOnFormSubmit.bind(this);
-    }
+    /**
+     * Used to altar the password text input
+     */
+    const [showPassword, setshowPassword] = React.useState(false);
 
     /**
      * Updates username state
      */
-    handleOnChangeUsername(event) {
-        this.setState({username: event.target.value})
+    const handleOnChangeUsername = (event) => {
+        setUsername(event.target.value)
     }
 
     /**
      * Updates password state
      */
-    handleOnChangePassword(event) {
-        this.setState({password: event.target.value})
+    const handleOnChangePassword = (event) => {
+        setPassword(event.target.value)
     }
 
     /**
      * Toggles password visibility
      */
-    handleClickShowPassword() {
-        this.setState({showPassword: !this.state.showPassword})
+    const handleClickShowPassword = () => {
+        setshowPassword(!showPassword)
     }
 
     /**
      * Prevents default behaviour
      */
-    handleMouseDownPassword(event) {
+    const handleMouseDownPassword = (event) => {
         event.preventDefault()
     }
 
@@ -58,70 +60,65 @@ export default class Login extends Component {
      * Submits a login request to authenticate the user
      * @TODO
      */
-    handleOnFormSubmit(event) {
+    const handleOnFormSubmit = (event) => {
         event.preventDefault()
     }
 
-    /**
-     * Render
-     */
-    render() {
-        return (
-            <Paper>
-                <Box p={3}>
-                    <Typography
-                        align="center"
-                        paragraph={true}
-                        style={{fontWeight: 500}}
-                    >
-                        Please enter your account information
-                    </Typography>
-                    <form onSubmit={this.handleOnFormSubmit}>
-                        <FormControl fullWidth={true} variant="outlined" margin="normal">
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <OutlinedInput
-                                label="Username"
-                                id="username"
-                                type="text"
-                                value={this.state.username}
-                                onChange={this.handleOnChangeUsername}
-                                className="login-input"
-                            />
-                        </FormControl>
-                        <FormControl fullWidth={true} variant="outlined" margin="normal">
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <OutlinedInput
-                                label="Password"
-                                id="password"
-                                type={this.state.showPassword ? 'text' : 'password'}
-                                value={this.state.password}
-                                onChange={this.handleOnChangePassword}
-                                className="login-input"
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={this.handleClickShowPassword}
-                                            onMouseDown={this.handleMouseDownPassword}
-                                        >
-                                            {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        </FormControl>
-                        <Box mt={2}>
-                            <Button
-                                onClick={this.handleOnFormSubmit}
-                                variant="contained"
-                                color="primary"
-                            >
-                                Login
-                            </Button>
-                        </Box>
-                    </form>
-                </Box>
-            </Paper>
-        )
-    }
+    return (
+        <Paper>
+            <Box p={3}>
+                <Typography
+                    align="center"
+                    paragraph={true}
+                    style={{fontWeight: 500}}
+                >
+                    Please enter your account information
+                </Typography>
+                <form onSubmit={handleOnFormSubmit}>
+                    <FormControl fullWidth={true} variant="outlined" margin="normal">
+                        <InputLabel htmlFor="username">Username</InputLabel>
+                        <OutlinedInput
+                            label="Username"
+                            id="username"
+                            type="text"
+                            value={username}
+                            onChange={handleOnChangeUsername}
+                            className="login-input"
+                        />
+                    </FormControl>
+                    <FormControl fullWidth={true} variant="outlined" margin="normal">
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <OutlinedInput
+                            label="Password"
+                            id="password"
+                            type={showPassword ? 'text' : 'password'}
+                            value={password}
+                            onChange={handleOnChangePassword}
+                            className="login-input"
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                    <Box mt={2}>
+                        <Button
+                            onClick={handleOnFormSubmit}
+                            variant="contained"
+                            color="primary"
+                        >
+                            Login
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+        </Paper>
+    )
 }
