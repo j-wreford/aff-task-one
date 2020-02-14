c74b6ef008ce662994e9cbaf8594ff1a12b0a925
Joe <joe.wreford@outlook.com>
2020-02-14 17:43:07 +0000

    Add MediaUpload.

---
+++ frontend/src/component/MediaUpload.js
@@ -0,0 +1,16 @@
+// react
+import React from 'react'
+
+// material ui
+import { Typography } from '@material-ui/core'
+
+/**
+ * Presents an interface to let the logged in user upload a new document
+ * to the database.
+ */
+export default function MediaUpload(props) {
+    console.log("Upload media");
+    return (
+        <Typography variant="h5">Upload your media</Typography>
+    )
+}
