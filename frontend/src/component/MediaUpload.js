ba57ca15afb802bae1d4f3980f3b318605c769d8
Joe <joe.wreford@outlook.com>
2020-02-15 16:12:54 +0000

    Update MediaUpload.

--- frontend/src/component/MediaUpload.js
+++ frontend/src/component/MediaUpload.js
@@ -4,6 +4,9 @@ import React from 'react'
 // material ui
 import { Typography } from '@material-ui/core'
 
+// application
+import PaddedPaper from '../component/hoc/PaddedPaper'
+
 /**
  * Presents an interface to let the logged in user upload a new document
  * to the database.
@@ -11,6 +14,11 @@ import { Typography } from '@material-ui/core'
 export default function MediaUpload(props) {
     console.log("Upload media");
     return (
-        <Typography variant="h5">Upload your media</Typography>
+        <React.Fragment>
+            <Typography variant="subtitle1" gutterBottom>Upload your media</Typography>
+            <PaddedPaper>
+                <Typography variant="p">Follow the instructions to upload a new document to the management system.</Typography>
+            </PaddedPaper>
+        </React.Fragment>
     )
 }
