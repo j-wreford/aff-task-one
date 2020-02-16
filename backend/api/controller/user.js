701457b869a33e9303f82f5b5dcccef79324677c
Joe <joe.wreford@outlook.com>
2020-02-16 16:21:42 +0000

    Refactor user.js register controller to utilise async/await.

--- backend/api/controller/user.js
+++ backend/api/controller/user.js
@@ -47,50 +47,49 @@ const userController = {
             reply.error = error
             reply.message = "We couldn't log you in. Please make sure your username and password is correct"
         }
-        
+
         response.json(reply)
     },
 
     /**
      *  Saves a new user to the user collection.
      * 
-     * response.data is an empty object.
+     * response.data is a user object stripped of its password field.
      */
-    register: (request, response) => {
+    register: async (request, response) => {
 
         let reply = defaultResponse({})
 
         try {
 
             const user = new models.UserAccount(request.body)
+            let saved = await user.save()
 
-            user.save().then(
+            reply.message = "Successfully created your account"
 
-                // success
-                user => {
+            // NOT WORKING
+            if (delete saved.password)
+                reply.data = saved
 
-                    reply.message = "Successfully created your account"
-                    reply.data = {}
-
-                    response.status(200).json(reply)
-                },
-
-                // failure
-                error => {
-
-                    reply.error = error
-                    reply.message = "Something went wrong while trying to create your account"
-
-                    response.status(400).json(reply)
-                }
-            )
-        } catch(error) {
+            response.status(200)
+        }
+        catch (error) {
 
             reply.error = error
-            reply.message = "Something went wrong while trying to create your account"
+            reply.message = "Something went wrong when trying to create your account"
+            response.status(500)
 
-            response.status(500).json(reply)
+            if (error && error.code) {
+
+                // E11000 duplicate key error collection
+                if (error.code === 11000) {
+                    reply.message = `An account already exists with the username ${request.body.userName}`
+                    response.status(200)
+                }
+            }
         }
+
+        response.json(reply)
     }
 }
 
