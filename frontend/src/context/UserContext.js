4d696abad11ebbbe7d5d7572cce7ecbefe107d10
Joe <joe.wreford@outlook.com>
2020-02-18 01:48:40 +0000

    Update UserContext.js.

--- frontend/src/context/UserContext.js
+++ frontend/src/context/UserContext.js
@@ -5,7 +5,7 @@ import React from 'react'
 import axios from 'axios'
 
 // cookie parsing
-import Cookies, { useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie'
 
 // application
 import constants from '../constants'
