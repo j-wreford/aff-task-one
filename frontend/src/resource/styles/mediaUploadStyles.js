3b36d83ba9df818fa4fff240d27eb9aebd251b79
Joe <joe.wreford@outlook.com>
2020-02-18 14:39:29 +0000

    Add mediaUploadStyles.

---
+++ frontend/src/resource/styles/mediaUploadStyles.js
@@ -0,0 +1,12 @@
+"use strict"
+
+import { makeStyles } from '@material-ui/core/styles'
+
+export default makeStyles(theme => ({
+    tagsBox: {
+        marginTop: "1em"
+    },
+    tag: {
+        margin: theme.spacing(0.5)
+    }
+}))
